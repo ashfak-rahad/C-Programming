beed2a64e5acaba188097ed5caff8be1627a5e99
ashfak-rahad <ahsfakur@gmail.com>
2023-12-20 22:59:28 +0600

    for loop

---
+++ for_loop.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+// int main(){
+//     int i;
+//     for (i = 100; i >=0; i=i-10)
+//     {
+//        printf("%d \n",i);
+//     }
+    
+//     return 0;
+// }
+#include<stdio.h>
+int main(){
+    int i;
+    for ( i = 0; i <=100 ; i=i+10)
+    {
+        printf("%d\n",i);
+    }
+    
+}
