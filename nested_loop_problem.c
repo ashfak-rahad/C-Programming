0ccbe7278a95ee9609569395f22ef26182e5e4b7
ashfak-rahad <ahsfakur@gmail.com>
2023-12-31 18:44:42 +0600

    nested loop problem solved

---
+++ nested_loop_problem.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+     int i,j,k;
+     for ( i = 1; i <=1; i++)
+     {
+        printf("*\n");
+        for ( j = 1; j <=1; j++)
+        {
+           printf("* *\n") ;
+           for ( i = 1; i <=1; i++)
+           {
+            printf("* * *\n");
+            for ( i = 1; i <=1; i++)
+            {
+               printf("* * * *\n");
+               for ( i = 1; i <=1; i++)
+               {
+                printf("* * * * *");
+               }
+               
+            }
+            
+           }
+           
+        }
+        printf("\n");
+        
+     }
+     
+    return 0;
+}
