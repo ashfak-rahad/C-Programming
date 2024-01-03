32ed21d1da1cd8093afba44a09ca796acaae7f48
ashfak-rahad <ahsfakur@gmail.com>
2024-01-03 22:08:37 +0600

    problem 2

---
+++ Problems/p2.c
@@ -0,0 +1,31 @@
+// Write a C program to print a block F using the hash (#), 
+// where the F has a height of six characters and width of five and four characters. 
+// And also print a very large 'C'.
+#include<stdio.h>
+int main(){
+     // Print a line of hashes
+    printf("######\n");
+    
+    // Print a single hash
+    printf("#\n");
+    
+    // Print a single hash
+    printf("#\n");
+    
+    // Print a line of hashes
+    printf("#####\n");
+    
+    // Print a single hash
+    printf("#\n");
+    
+    // Print a single hash
+    printf("#\n");
+    
+    // Print a single hash
+    printf("#\n");
+
+    
+
+
+    return 0;
+}
