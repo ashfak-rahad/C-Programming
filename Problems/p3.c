471d976d42a7182bf7abfc0a7537522e9b9d616a
ashfak-rahad <ahsfakur@gmail.com>
2024-01-03 22:18:07 +0600

    ascii problem

---
+++ Problems/p3.c
@@ -0,0 +1,12 @@
+/*Write a program to print the following output:
+Ascii value of "A" is:
+Ascii value of "@" is:
+*/
+
+#include<stdio.h>
+int main()
+{
+  printf("Ascii value of 'B' is: %d\n",'B');
+  printf("Ascii value of '@' is: %d\n",'@');
+    return 0;
+}
