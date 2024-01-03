070401a3e9a2e625ccfc476dac66417e7e62ce12
ashfak-rahad <ahsfakur@gmail.com>
2024-01-03 21:36:18 +0600

    problem 1

---
+++ Problems/p1.c
@@ -0,0 +1,10 @@
+// 1. Write a C program to print your name,
+//  date of birth, and mobile number.
+#include<stdio.h>
+int main(){
+     printf("Name  \t \t:Ahsfakur Rahad\n");
+     printf("DOB \t \t:July 14, 1997\n");
+     printf("Mobile \t \t:01778-62016*\n");
+
+    return 0;
+}
