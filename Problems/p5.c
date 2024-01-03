3b233ab9758c99f485ea2d6c439370d780e23216
ashfak-rahad <ahsfakur@gmail.com>
2024-01-03 22:35:00 +0600

    Bangladesh

---
+++ Problems/p5.c
@@ -0,0 +1,9 @@
+/*Print "Bangladesh" without quotation sign by using ascii values*/
+
+#include<stdio.h>
+int main()
+{
+    int B=66,a=97,n=110,g=103,l=108,A=97,d=100,e=101,s=115,h=104 ;
+    printf("%c%c%c%c%c%c%c%c%c%c",B,a,n,g,l,A,d,e,s,h);
+    return 0;
+}
